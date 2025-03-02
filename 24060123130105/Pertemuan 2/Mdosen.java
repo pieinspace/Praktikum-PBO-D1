2befa0d6ffeaa9a21b501c0885bd558807de6daf
Nadzif Silakhudin <nadzifsilakhudin@students.undip.ac.id>
2025-03-02 23:26:57 +0700

    Tugas pertemuan 2 Nadzif | 24060123130105

---
+++ 24060123130105/Pertemuan 2/Mdosen.java
@@ -0,0 +1,9 @@
+public class Mdosen {
+    public static void main(String[] args) {
+        
+        Dosen d1;
+
+        d1 = new Dosen("yanto", "yanti", "rahmat");
+        System.out.println(d1.getNama());
+    }
+}
